00072784b48bd77de953ae8cdb988d1ff7fb3373
Oleksandr Hiliazov <oleksandr.hiliazov@robivision.eu>
2024-03-19 10:45:28 +0100

    Some good changes

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,29 @@
 from datetime import datetime
 
+from django.contrib.auth.models import User
 from django.forms.models import model_to_dict
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .forms import UserProfileForm
 
 
 def user_profile(request):
+    if not request.user.is_authenticated:
+        return redirect("/")
+
     if request.method == "POST":
         form = UserProfileForm(request.POST)
         if form.is_valid():
             obj = form.save(commit=False)
             obj.user_id = request.user.id
             obj.save()
-    elif request.user and request.user.userprofile:
-        form = UserProfileForm(data=model_to_dict(request.user.userprofile))
     else:
-        form = UserProfileForm()
+        try:
+            form = UserProfileForm(
+                data=model_to_dict(request.user.userprofile)
+            )
+        except User.userprofile.RelatedObjectDoesNotExist:
+            form = UserProfileForm()
 
     now = datetime.now()
     return render(
